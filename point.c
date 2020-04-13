05ec3255e3a31affe63719bd02f7e4d19e4da1cb
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2020-04-13 07:39:08 +0530

    get_closest function: added a for-loop which loop through the food points and return last food points. defined the min_distace and distace variable.

--- point.c
+++ point.c
@@ -7,10 +7,17 @@ double find_distance(struct Point food_points,struct Point current_location){
 
 void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location){
   double min_distance = __DBL_HAS_INFINITY__;
+  double distance = 0;
+
   for (int i = ZERO; i < points_length; i++){
-    find_distance(food_points[i],current_location);
+    distance = find_distance(food_points[i],current_location);
+
+    if(distance < min_distance){
+      closest_food_location->x = food_points[i].x;
+      closest_food_location->y = food_points[i].y;
+    }
+    
   }
 
-  closest_food_location->x = food_points[ZERO].x;
-  closest_food_location->y = food_points[ZERO].y;
+  
 }
